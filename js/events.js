866cb04d1beb22eb92b446695f5ce8721ffb2222
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-31 18:14:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,9 +12,15 @@ function frameIt() {
 
 function pressIt() {
   $('input').on('keydown', function(key) {
-    if(key.which === ) {
+    if(key.which === 71) {
       alert('G was pressed.');
     }
+  });
+}
+
+function submitIt() {
+  $("form").on('submit', function() {
+    alert("Your form is going to be submitted now.")
   });
 }
 
